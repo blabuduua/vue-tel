0bffd2f4034f3abb1dd26f12d271c484629d027f
blabuduua <31963350+blabuduua@users.noreply.github.com>
2022-06-17 12:20:46 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Vuetify template</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-alert dense text type="info"
+          >Phone text field using 'v-text-field'</v-alert
+        >
+        <v-text-field outlined label="Phone" v-model="phone"> </v-text-field>
+
+        <v-alert dense text type="info"
+          >Phone text field using 'vue-tel-input-vuetify'</v-alert
+        >
+        <vue-tel-input-vuetify
+          outlined
+          label="Phone"
+          v-model="phone"
+        ></vue-tel-input-vuetify>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      phone: null,
+    };
+  },
+};
+</script>
